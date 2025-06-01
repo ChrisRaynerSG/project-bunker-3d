c930bd55d2fba0d12059667215afcf2753732c66
Chris Rayner <crayner@spartaglobal.com>
2025-06-01 10:12:01 +0100

    Add code to only rebuild the chunk effected by mine action - NOT IMPLEMENTED need to fix as currently does not work, add to todo for future optimisation (currently runs fine rebuilding the entire layer but will likely change as more is added to the game...

--- Assets/_Scripts/Simulation/Mining/BlockAccessor.cs
+++ Assets/_Scripts/Simulation/Mining/BlockAccessor.cs
@@ -9,23 +9,6 @@ public class BlockAccessor
         this.world = world;
     }
 
-    // public void SetBlock(Vector3Int position, BlockData.BlockType blockType)
-    // {
-    //     ChunkData chunk = world.GetChunkAtWorldPosition(position.x, position.z);
-    //     if (chunk != null)
-    //     {
-    //         int localX = position.x % ChunkData.CHUNK_SIZE;
-    //         int localZ = position.z % ChunkData.CHUNK_SIZE;
-
-    //         if (localX < 0) localX += ChunkData.CHUNK_SIZE;
-    //         if (localZ < 0) localZ += ChunkData.CHUNK_SIZE;
-
-    //         BlockData block = chunk.Grid[localX][localZ];
-    //         block.BlockTypeName = blockType.ToString();
-    //         block.IsSolid = blockType != BlockType.Air; // Example logic for solid blocks
-    //     }
-    // }
-
     public void SetBlock(Vector3Int position, BlockData.BlockType blockType)
     {
         BlockData block = GetBlock(position);
@@ -33,13 +16,16 @@ public class BlockAccessor
         {
             block.BlockTypeName = blockType.ToString();
             block.IsSolid = blockType != BlockData.BlockType.Air;
-            
+
             world.RebuildMeshAtLevel(position.y);
             world.RebuildMeshAtLevel(position.y + 1); // Rebuild the mesh at the block's Y level
             world.RebuildMeshAtLevel(position.y - 1); // Rebuild the mesh at the block's Y level
-        }
 
-    
+            // attempt to reubild only the chunk that contains the block - might not work with adjacent chunks...
+            // world.RebuildChunkMesh(world.GetChunkAtPosition(position.x, position.y, position.z));
+            // world.RebuildChunkMesh(world.GetChunkAtPosition(position.x, position.y + 1, position.z));
+            // world.RebuildChunkMesh(world.GetChunkAtPosition(position.x, position.y - 1, position.z));
+        }
     }
 
     public BlockData GetBlock(Vector3Int position)

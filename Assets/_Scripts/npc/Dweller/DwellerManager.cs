ef17219c17cc8645b3803449b3647ea4f54ccf7c
Chris Rayner <crayner@spartaglobal.com>
2025-06-06 23:33:07 +0100

    Update DwellerManager with basic CRUD operations - may want to change this to something a bit more comprehensive later on, not sure if this is the right route to go with unity...

--- Assets/_Scripts/npc/Dweller/DwellerManager.cs
+++ Assets/_Scripts/npc/Dweller/DwellerManager.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 
 public class DwellerManger : MonoBehaviour
 {
-    List<Dweller> dwellers = new();
-
+    private List<Dweller> dwellers = new();
     public void AddDweller(Dweller dweller)
     {
 
     }
+
     public void RemoveDweller(string dwellerId)
     {
 
     }
-
     public Dweller GetDweller(string dwellerId)
     {
         return null;
+    }
+
+    public List<Dweller> GetAllDwellers()
+    {
+        return dwellers;
+    }
+    public void CreateDweller(string dwellerId, string name, Vector3 position)
+    {
+        Dweller newDweller = new Dweller();
+        AddDweller(newDweller);
+    }
+
+    public void UpdateDweller()
+    {
+        //probably dont need this but might want to have it? update dweller logic within dwellers themselves? not sure the best way to handle this
+        
     }
 }

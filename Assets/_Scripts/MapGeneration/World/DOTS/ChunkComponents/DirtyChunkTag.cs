b1b6e379204120cda0f90da7b1ee82ac65122773
Chris Rayner <crayner@spartaglobal.com>
2025-06-10 17:03:30 +0100

    remove enablable tags for the most part at world generation, structural changes this early on probably aren't going to be huge and I broke absolutely everything. May want to revisit at a later point if world gen starts taking a very long time

--- Assets/_Scripts/MapGeneration/World/DOTS/ChunkComponents/DirtyChunkTag.cs
+++ Assets/_Scripts/MapGeneration/World/DOTS/ChunkComponents/DirtyChunkTag.cs
@@ -1,5 +1,5 @@
 using Unity.Entities;
-public struct DirtyChunkTag : IComponentData , IEnableableComponent
+public struct DirtyChunkTag : IComponentData, IEnableableComponent
 {
     // This tag is used to mark chunks that need to be updated or processed.
     // It can be used in systems to filter out chunks that have been modified.
--- Assets/_Scripts/MapGeneration/World/DOTS/WorldComponents/ChunksGeneratedTag.cs
+++ Assets/_Scripts/MapGeneration/World/DOTS/WorldComponents/ChunksGeneratedTag.cs
@@ -1,5 +1,5 @@
 using Unity.Entities;
-public struct ChunksGeneratedTag : IComponentData , IEnableableComponent
+public struct ChunksGeneratedTag : IComponentData
 {
     // This tag is used to denote that chunks have been generated
 }
